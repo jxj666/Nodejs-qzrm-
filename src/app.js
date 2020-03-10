38d7a7f2a5aa10a9d0c6ff32057665115fe2df19
jinxiaojian <jinxiaojian@youxin.com>
2020-03-11 00:05:18 +0800

    fix

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
-  res.end("hello");
+  res.end("hello world");
 });
-server.listen(3001, "127.0.0.1", () => {
+server.listen(3000, "127.0.0.1", () => {
   console.log("server 启动");
 });
